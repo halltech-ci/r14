ba7824a50fcb18925e612c1961ccae1588c90eff
halltech-ci <odoo@eupd2.odoo.com>
2020-10-22 20:54:39 +0000

    Adding sale_order in mrp_production_view

--- hta_custom_mrp/__manifest__.py
+++ hta_custom_mrp/__manifest__.py
@@ -10,19 +10,19 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "ATCHE Maurice",
+    'website': "http://www.halltech-africa.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Manufacturing/Manufacturing',
-    'version': '0.1',
+    'version': '14.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base_setup',
                 'mrp',
-               #'sale_management',
+                'sale_management',
                ],
 
     # always loaded
@@ -30,6 +30,7 @@
          #'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/mrp_production_views.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
