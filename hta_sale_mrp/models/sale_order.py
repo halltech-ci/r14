8a8ddcb5aeb8ff6c33c0df302b396679ddebd79e
halltech-ci <odoo@eupd2.odoo.com>
2020-10-27 23:52:14 +0000

    Adding create mrp button in sale order

--- hta_sale_mrp/models/sale_order.py
+++ hta_sale_mrp/models/sale_order.py
@@ -6,8 +6,24 @@ from odoo.addons import decimal_precision as dp
 class SaleOrder(models.Model):
     _inherit = "sale.order"
     
-   
+    """
+    Create BOM associated to the SO. 
+    """
+    def create_product_template(self):
+        prod_template = self.env['product.template']
+        #category_id = 
+        for rec in self:
+            product_name = rec.name + '-' + rec.description
+            product = prod_template.create({
+                'name': product_name,
+                'purchase_ok': False,
+                'type': 'product',
+                
+            })
+    
     def action_create_mrp(self):
         bom = self.env["mrp.bom"]
+        
+            
         
     
