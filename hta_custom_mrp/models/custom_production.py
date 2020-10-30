22c78767f7d836fb7410cff904bf78a1a89e6361
halltech-ci <odoo@eupd2.odoo.com>
2020-10-30 00:59:06 +0000

    Adding custom bom in hta_sale_mrp

--- hta_custom_mrp/models/custom_production.py
+++ hta_custom_mrp/models/custom_production.py
@@ -14,5 +14,12 @@ class CustomProduction(models.Model):
     #sale_order = fields.Many2one('sale.order', string="Devis")
     name = fields.Char('Reference', copy=False, readonly=True, default=lambda x: _('New'))
     description = fields.Text("Description")
+    sale_order = fields.Many2one('sale.order', string='Sale Order')
+    
+    
+class CsutomProductionLine(model.Models):
+    _name = "custom.production.line"
+    
+    
     
     
--- hta_sale_mrp/models/sale_order.py
+++ hta_sale_mrp/models/sale_order.py
@@ -7,54 +7,55 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
     
     description = fields.Text('Description')
+    sale_mrp_product = fields.Many2one('product.template', ondelete='cascade')
+    mrp_bom_id = fields.Many2one("mrp.bom")
+    
     """
     Create BOM associated to the SO. 
     """
-    def action_create_bom(self):
-        #create product
-        prod = self.env['product.template']
-        for record in self:
-            product_name = record.description
+    def create_sale_mrp_product(self, order):
+        '''create product template for coresponding sale_order'''
+        product_template = self.env['product.template']
+        if not order.sale_mrp_product:
+            product_name = order.description
             route_id = self.env['stock.location.route'].search([('name', '=', 'Manufacture')]).id
             route = []
             route_ids = route.append(route_id)
-            prod.create({
+            product_template.create({
                 'name': product_name,
                 'type': 'product',
                 'purchase_ok': False,
                 'categ_id': 1,
                 'route_ids': route_ids
             })
-    
-    
-    def create_product_template(self):
-        prod_template = self.env['product.template']
-        #category_id = 
-        for rec in self:
-            product_name = rec.name + '-' + rec.description
-            product = prod_template.create({
-                'name': product_name,
-                'purchase_ok': False,
-                'type': 'product',
-                #categ_id = 1
+            order.sale_mrp_product = product_template
+            
+    def create_product_bom(self, order):
+        bom_id = self.env['product.template'].search([('id', '=', order.sale_mrp_product)], limit=1)
+        bom = self.env['mrp.bom']
+        bom_line = self.env['mrp.bom.line']
+        product_ids = []
+        lines = order.order_line
+        if not order_id.mrp_bom_id:
+            bom.create({
+                'product_templ_id': order.sale_mrp_product,
+            })
+            order.mrp_bom_id = bom
+            for line in lines:
+                product_ids.append(line.product_id)
+                bom_line.create({
+                    'product_id': line.product_id.id,
+                    'product_qty': line.product_uom_qty.id,
+                    'bom_id':bom.id
+                })
                 
-            })
-            
-    def create_product_category(self):
-        categ = self.env['product.category']
-        categ_name = self.env['product.category'].search([('name', '=', 'Manufacture')])
-        if not categ_name:
-            categ.create({
-                'name': 'Manufacture',
-                'parent_id': 1,
-                'property_cost_method': 'standard',
-                'propert_valuation': 'namual_periodic'
-            })
-            
     #@api.model
-    def action_create_mrp(self, bom_id):
-        self.create_product_category
-        bom = self.env["mrp.bom"]
+    def action_create_mrp(self):
+        mo_id = self.env['mrp.production']
+        bom = self.env['mrp.bom']
+        
+            
+                
         
             
         
